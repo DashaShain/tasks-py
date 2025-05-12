77e69232832680a51bd780cf9cfb620ca9d74dd7
DashaShain <d.shainova@yandex.ru>
2025-05-12 23:18:46 +0300

    Add files via upload

--- task4.py
+++ task4.py
@@ -1,28 +1,33 @@
 class FlattenIterator:
     def __init__(self, nested_list):
-        self.stack = [nested_list]       # Стек списков
-        self.index_stack = [0]           # Индексы внутри каждого уровня
+        self.stack = [(nested_list, 0)]
 
     def __iter__(self):
         return self
 
     def __next__(self):
         while self.stack:
-            current_list = self.stack[-1]
-            index = self.index_stack[-1]
+            current_list, index = self.stack[-1]
 
             if index >= len(current_list):
                 self.stack.pop()
-                self.index_stack.pop()
                 continue
 
-            self.index_stack[-1] += 1
+            self.stack[-1] = (current_list, index + 1)
             item = current_list[index]
 
             if isinstance(item, list):
-                self.stack.append(item)
-                self.index_stack.append(0)
+                self.stack.append((item, 0))
             else:
                 return item
 
         raise StopIteration
+
+
+class FlattenIterable:
+    def __init__(self, nested_list):
+        self.nested_list = nested_list
+
+    def __iter__(self):
+        return FlattenIterator(self.nested_list)
+        
